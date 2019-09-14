b92c2aecf4c4a44ac548f0552fe68a6910f97a32
AlFalah <taiebalfalah@gmail.com>
2019-09-14 07:11:58 +0200

    Not bad as first attempt

--- auto.sh
+++ auto.sh
@@ -6,7 +6,7 @@
 
 getInfo(){
 
-    result= curl -s  "http://data.fixer.io/api/latest?access_key=$APIKEY&format=1/" | jq '.timestamp,.base,.rates.TND'
+    result= curl -s  "http://data.fixer.io/api/latest?access_key=$APIKEY&format=1/" | jq '.timestamp,.base,.rates.TND,.rates.USD'
     # echo "$id"
     # echo $result | jq -r '.rates.TND' 
 
@@ -15,15 +15,26 @@ getInfo(){
 }
 
 extract(){
+    now=`date`
+    # file= test.json
     # test= cat test.json | jq '.timestamp,.base,.rates.TND, .rates.USD' 
-    timeupdate=  test.json |  jq '.timestamp'
-    base= cat test.json |  jq '.base'
-    tnd= cat test.json |  jq '.rates.TND'
-    usd= cat test.json |  jq '.rates.USD'
+    timeupdate=$(head -n 1 test.json)
+    base=$(sed '2q;d' test.json)
+    tnd=$(sed '3q;d' test.json)
+    usd=$(sed '4q;d' test.json)
+    # echo $base
 
+   echo  "## Current date: $now\n "  "# Monaie de base : "$base "\n" "# Equivaut en 🇹🇳 à : "$tnd > test.md
+#     base= cat test.json |  jq '.base'
+#     tnd= cat test.json |  jq '.rates.TND'
+#     usd= cat test.json |  jq '.rates.USD'
+# bash 
+    # echo $base
+    # echo $timeupdate
     # echo $base
 }
-getInfo
+
+# getInfo
 # getInfo > test.json
 
-# extract
+extract
---
+++ test.md
@@ -0,0 +1,3 @@
+## Current date: samedi 14 septembre 2019, 06:56:08 (UTC+0200)
+  # Monaie de base : "EUR" 
+ # Equivaut en 🇹🇳 à : 3.19304
